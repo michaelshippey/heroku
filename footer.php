b213a3ec0e6fd686e1e7946f0f9c762e11c14182
Michael Shippey <46725175+michaelshippey@users.noreply.github.com>
2020-04-06 08:52:08 -0600

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-</body>
-    <footer>
-        <div class = "footer">
-            <li><a>&copy; 2020 Michael Shippey Media | Intro to Web Development</a></li>
-        </div>
-    </footer>
-</html>

4fc87f69af53af5628228a4dfebb24e834181eb8
michaelshippey@u.boisestate.edu <michaelshippey@u.boisestate.edu>
2020-04-06 17:48:47 -0600

    restarting again

--- login.php
+++ login.php
@@ -1,34 +1,9 @@
 <?php 
 session_start();
 
+$username = $_POST['myname'];
+$password = $_POST['mypword'];
 
-$firstname = $_POST['fname'];
-$lastname = $_POST['lname'];
-$email= $_POST['email'];
-$email2 = $_POST['email2'];
-$username = $_POST['username'];
-$password = $_POST['password'];
-$password2 = $_POST['password2'];
-
-
-
-
-
-require_once 'Dao.php';
-
-try {
-    $dao = new Dao();
-    $dao->getConnection();
-    $dao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $dao->login($_POST['username'], $_POST['password']);
-    header("Location: https://michaelshippey.herokuapp.com/login.php");
-    exit;
-} catch(PDOException $Exception){
-    throw new MyDatabaseException( $Exception->getMessage( ) ,
-    (int)$Exception->getCode( ) );
-}
-    
 
 ?>
 <?php 

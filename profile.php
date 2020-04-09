163421aafab71f736caac20156f68fa1b5a1ac9f
michaelshippey@u.boisestate.edu <michaelshippey@u.boisestate.edu>
2020-04-09 12:46:24 -0600

    styling posts

--- profile.php
+++ profile.php
@@ -53,7 +53,7 @@ if (isset($_SESSION['postForm'])) {
          $lines = $dao->getPosts($_SESSION['username']);
          while($line = $lines->fetch(PDO::FETCH_ASSOC)) {
             extract($line);
-            echo sprintf('<li> Posted by: %s </li>' ,$username);
+            echo sprintf('<li> Posted by: %s </li> </br>' ,$username);
             echo sprintf('<li> Post Content: %s </li>' ,$content);
             echo sprintf('<li> Posted at: %s </li>' ,$date_entered);
          }

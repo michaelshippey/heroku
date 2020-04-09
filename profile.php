0f3bf4150bf604baf1906c1deef2e41087c584ca
michaelshippey@u.boisestate.edu <michaelshippey@u.boisestate.edu>
2020-04-08 21:33:09 -0600

    posts

--- profile.php
+++ profile.php
@@ -8,8 +8,7 @@ if (!isset($_SESSION['auth']) || !$_SESSION['auth'])  {
 
 include_once("profileHeader.php");
 
-require_once 'Dao.php';
-$dao = new Dao();
+
 
 $post_preset = "";
 
@@ -30,39 +29,13 @@ if (isset($_SESSION['postForm'])) {
    
     </div>
     <div class = "profilePosts"> Your Posts here ... 
-    <?php
-         $lines = $dao->getPosts($_SESSION['username']);
-            if (is_null($lines)) {
-                    echo "There was an error.";
-            } else {
-         foreach ($lines as $line) {
-           echo "<div> $line['username'] </div>" . "<div> $line['content'] </div>" . "<div> $line['date_entered'] </div>";
-          }
-      }
-      sleep(2);
-    ?>
+
 
     </div>
     <img src="profile_Picture.png" height = "250" width = "200" alt="<?php echo $_SESSION['username']; ?>'s Profile:" title="<?php echo $_SESSION['username']; ?>'s Profile" />
     <br />
     <div class = "textHeader"><? echo $_SESSION['username']; ?>'s Profile</div>
 
-
-
-    <?php 
-
-            if (isset($_SESSION['errors2'])) {
-                foreach ($_SESSION['errors2'] as $errors2) {
-                    echo "<div id='error'>{$errors2}</div>";
-                }
-                unset($_SESSION['errors2']);
-            }
-            if (isset($_SESSION['successPost'])) {
-                echo "<div id='success'>{$_SESSION['successPost']}</div>";
-                unset($_SESSION['successPost']);
-            }
-                            
-    ?>
 
     </div>
 

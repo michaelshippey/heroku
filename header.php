ebe8b04bc8bc4d79982d409902c5c4d827c92725
michaelshippey@u.boisestate.edu <michaelshippey@u.boisestate.edu>
2020-04-22 17:12:25 -0600

    jquery

--- header.php
+++ header.php
@@ -11,19 +11,19 @@
                 <div class = logo>
                     <a href = "register.php"> <img src = "logo.png"/> </a>
                 </div>
-                <div id="navBar">
+                <nav id="navBar">
                     <a href ="register.php"> Home </a>
                     <a href ="profile.php"> Profile </a>
                     <a href ="register.php"> Sign Up</a>
                     <a href ="login.php"> Sign In </a>
-                </div>
+                </nav>
             </div>
         </div>
 
         <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 <script>
     var url = window.location.href.split("/"); //replace string with location.href
-    var navLinks = document.getElementsByTagName("navBar")[0].getElementsByTagName("a");
+    var navLinks = document.getElementsByTagName("nav")[0].getElementsByTagName("a");
     //naturally you could use something other than the <nav> element
     var i=0;
     var currentPage = url[url.length - 1];
